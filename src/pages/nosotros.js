3ae2814d27824541f8e800fd44a441a01e4bff08
BrandonOsorio <jesusw52@gmail.com>
2020-03-03 16:35:08 -0600

    Primer commit

--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Layout from '../components/Layout';
 
-const IndexPage = () => (
+const Nosotros = () => (
   <Layout>
-    <h1>Index</h1>
+    <h1>Nosotros</h1>
   </Layout>
 )
 
-export default IndexPage
+export default Nosotros
